e345236c1ff1190b759296a12a72cea7b62afe9a
Kaiser Ahmed <kaiserahmed.isu@gmail.com>
2017-04-30 13:28:45 -0500

    main app updated for hetoku

--- src/main/java/com/linkedu/it353/MainApplication.java
+++ src/main/java/com/linkedu/it353/MainApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
 		SpringApplication.run(MainApplication.class, args);
 	}
 }
